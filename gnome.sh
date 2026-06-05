b8ea1ff4eeb96760788351e2d4cf305141813219
Cleverson <SmileYzn@users.noreply.github.com>
2026-06-05 09:36:59 -0300

    Update APT sources to include contrib and non-free
    Added support for non-free firmware in APT sources.

--- gnome.sh
+++ gnome.sh
@@ -15,11 +15,27 @@ fi
 # Abrir pasta do usuário
 cd /home/$USUARIO
 
+# Adicionar contrib non-free-firmware
+set -e
+
+if [ -f /etc/apt/sources.list.d/debian.sources ]; then
+    sudo sed -i \
+        's/^Components:.*/Components: main contrib non-free non-free-firmware/' \
+        /etc/apt/sources.list.d/debian.sources
+else
+    sudo sed -Ei \
+        's#^(deb(-src)? .+ trixie(-security|-updates)? main).*$#\1 contrib non-free non-free-firmware#' \
+        /etc/apt/sources.list
+fi
+
 # Atualizar Respositórios
 sudo apt update
 
 # Atualizar Sistema
-sudo apt upgrade
+sudo apt upgrade -y
+
+# ??
+sudo apt install -y git
 
 # Fim
 exit
